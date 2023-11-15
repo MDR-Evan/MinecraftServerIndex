8916a32ecb891db9a7872c81975c4caa9d5dda2d
Jinyong <kjy040216@kbu.ac.kr>
2023-11-15 14:57:12 +0900

    update

--- js/server_checked.js
+++ js/server_checked.js
@@ -8,17 +8,25 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch(`https://api.mcsrvstat.us/2/${serverAddress}:${serverPort}`)
             .then(response => response.json())
             .then(data => {
-                const statusElements = document.querySelectorAll('.status');
+                const statusElement = document.getElementById('status'); // 수정된 부분: statusElements -> statusElement
                 if (data.online) {
-                    // statusElement.textContent =`${data.players.online}`;
-                    statusElement.style.backgroundColor = '#22E625';
-                    statusElement.style.borderRadius = "10px";
-                    statusElement.style.padding = "15px 15px";
+                    statusElement.textContent =`${data.players.online}`;
+                    statusElement.style.color = '#FFFFFF';
+                    statusElement.style.width = '94.63px';
+                    statusElement.style.height = '43px';
+                    statusElement.style.margin = '0';
+                    statusElement.style.padding = '0';
+                    statusElement.style.display = 'grid';
+                    statusElement.style.placeContent = 'center';  
                 } else {
-                    // statusElement.textContent = '서버가 오프라인 상태입니다.';
-                    statusElement.style.backgroundColor = 'red';
-                    statusElement.style.borderRadius = "10px";
-                    statusElement.style.padding = "15px 15px";
+                    statusElement.textContent = 'Offline';
+                    statusElement.style.color = '#E53C20';
+                    statusElement.style.width = '94.63px';
+                    statusElement.style.height = '43px';
+                    statusElement.style.margin = '0';
+                    statusElement.style.padding = '0';
+                    statusElement.style.display = 'grid';
+                    statusElement.style.placeContent = 'center';    
                 }
             })
             .catch(error => {
