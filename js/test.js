0a693dcc28a1974a5b34f24eaa8bc9e0a23c94e2
Jinyong <135928466+MDR-Evan@users.noreply.github.com>
2023-11-16 12:39:40 +0900

    update

--- js/test.js
+++ js/test.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Minecraft 서버의 주소와 포트를 설정합니다.
-    const serverAddress = '218.232.210.31';
+    // const serverAddress = '218.232.210.31';
     const serverPort = 25565;
+    const serverAddress = 'pixel.agpk.kr';
+
+    const checkButton = document.getElementById('checkButton'); // 예시: HTML에서 버튼의 ID를 'checkButton'으로 가정
     // 팝업창
     const wrapper = document.querySelector("#wrapper"),
     toast = wrapper.querySelector("#toast"),
     on_icon = wrapper.querySelector("#icon"),
     title = wrapper.querySelector("span"),
     subTitle = wrapper.querySelector("p");
+    close_icon = wrapper.querySelector("#close-icon");
 
     // Minecraft 서버 상태를 확인하는 함수
     function checkServerStatus() {
@@ -17,14 +21,36 @@ document.addEventListener('DOMContentLoaded', function() {
                 const statusElement = document.getElementById('wrapper');
                 if (data.online) {
                     toast.classList.add("online");
+                    toast.classList.remove("offline");
+                    wrapper.classList.remove("hide");
                     title.innerText = "Server Online";
                     subTitle.innerText = "어서 날래날래 접속하라우!";
                     on_icon.innerHTML = '<i class="fa-solid fa-toggle-on"></i>';
+                    toast.style.borderLeft = "5px solid #2ecc71";
+
+                    close_icon.onclick = ()=>{
+                        wrapper.classList.add("hide");
+                    }
+
+                    // setTimeout(() => {
+                    //     wrapper.classList.add("hide");
+                    // }, 10000);
                 } else {
                     toast.classList.add("offline");
+                    toast.classList.remove("online");
+                    wrapper.classList.remove("hide");
                     title.innerText = "Server Offline";
                     subTitle.innerText = "오정민 데려와ㅏㅏㅏ";
                     on_icon.innerHTML = '<i class="fa-solid fa-toggle-off"></i>';
+                    toast.style.borderLeft = "5px solid #e74c3c";
+
+                    close_icon.onclick = ()=>{
+                        wrapper.classList.add("hide");
+                    }
+
+                    // setTimeout(() => {
+                    //     wrapper.classList.add("hide");
+                    // }, 10000);
                 }
             })
             .catch(error => {
@@ -32,6 +58,11 @@ document.addEventListener('DOMContentLoaded', function() {
             });
     }
 
+    // checkServerStatus(); // 페이지 접속 할 떄 1회성 확인 
     // 정기적으로 서버 상태를 확인합니다.
-    setInterval(checkServerStatus, 5000); // 5초마다 확인합니다. 필요에 따라 변경하세요.
+    setInterval(checkServerStatus, 5000); // 5초마다 확인합니다. 필요에 따라 변경하세요. (1000 = 1s)
+
+    checkButton.addEventListener('click', function() {
+        checkServerStatus(); // 서버 상태 확인 함수 호출
+    });
 });
