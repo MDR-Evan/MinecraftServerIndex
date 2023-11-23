fc2fe54338e8b69c24c3a048dd40fdfe6b84bf9a
Jinyong <135928466+MDR-Evan@users.noreply.github.com>
2023-11-23 10:50:27 +0900

    update

--- js/server_checked.js
+++ js/server_checked.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Minecraft 서버의 주소와 포트를 설정합니다.
-    const serverAddress = '218.232.210.31';
+    // 오정민
+    // const serverAddress = '218.232.210.31'; 
+    // 이순주
+    const serverAddress = '210.221.63.232'; 
     const serverPort = 25565;
 
     // Minecraft 서버 상태를 확인하는 함수
@@ -10,23 +13,23 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(data => {
                 const statusElement = document.getElementById('status'); // 수정된 부분: statusElements -> statusElement
                 if (data.online) {
-                    statusElement.textContent =`${data.players.online}`;
-                    statusElement.style.color = '#FFFFFF';
-                    statusElement.style.width = '94.63px';
-                    statusElement.style.height = '43px';
-                    statusElement.style.margin = '0';
-                    statusElement.style.padding = '0';
+                    statusElement.textContent =`${data.players.online} / ${data.players.max}`;
                     statusElement.style.display = 'grid';
                     statusElement.style.placeContent = 'center';  
-                } else {
-                    statusElement.textContent = 'Offline';
+                    statusElement.style.textAlign = 'center';
+                } 
+                // else {
+                //     statusElement.textContent =`${data.players.online}`;
+                //     statusElement.style.display = 'grid';
+                //     statusElement.style.placeContent = 'center';  
+                //     statusElement.style.textAlign = 'center';
+                // }
+                else {
+                    statusElement.textContent = '금일 영업은 종료하였습니다.';
                     statusElement.style.color = '#E53C20';
-                    statusElement.style.width = '94.63px';
-                    statusElement.style.height = '43px';
-                    statusElement.style.margin = '0';
-                    statusElement.style.padding = '0';
                     statusElement.style.display = 'grid';
                     statusElement.style.placeContent = 'center';    
+                    statusElement.style.textAlign = 'center';
                 }
             })
             .catch(error => {
